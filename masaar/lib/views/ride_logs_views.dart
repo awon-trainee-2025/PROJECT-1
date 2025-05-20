763f7ef7c99ce143c0d21d6d267fe87f17214437
AhmadAbdullah123 <asindy200@gmail.com>
2025-05-20 15:50:13 +0300

    recommited

--- masaar/lib/views/ride_logs_views.dart
+++ masaar/lib/views/ride_logs_views.dart
@@ -8,12 +8,46 @@ class RideLogs extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Rides',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+          ),
+          backgroundColor: Colors.white,
+        ),
         backgroundColor: Colors.white,
         body: Padding(
           padding: EdgeInsets.only(top: 10),
           child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 31.0),
+                      child: Text(
+                        'Past',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        Divider(indent: 24, endIndent: 24),
+                        Divider(
+                          indent: 18.5,
+                          endIndent: 360,
+                          color: Color(0xFF6A42C2),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
                 CustomRideLog(
                   destination: 'CRR4+4X9، حسان بن ثابت، البيبان، مكة 24231',
                   status: 'Cancelled',
